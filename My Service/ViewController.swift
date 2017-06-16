5f160c7472a696e14fc622c7b4fd38c40b9a7fee
Student03 <phrombutr@gmail.comm>
2017-06-16 14:20:10 +0700

    Login Check Empty Input Success

--- My Service/ViewController.swift
+++ My Service/ViewController.swift
@@ -12,6 +12,41 @@ class ViewController: UIViewController {
 
    //Explicit
     var myString = ""
+    var strUser: String? = nil
+    var strPassword: String? = nil
+    
+    
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
+    @IBAction func loginButtom(_ sender: Any) {
+    
+        //Get Value Frome TextField
+        strUser = userTextField.text
+        strPassword = passwordTextField.text
+        
+        //Check Space
+        if strUser?.characters.count == 0 || strPassword?.characters.count == 0 {
+            alertLabel.alpha = 1
+        }   else {
+        
+            alertLabel.alpha = 0
+        }
+            
+
+    
+    
+    
+    }   //loginButton
+   
     
     
     
@@ -19,35 +54,66 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        //Process Connected Http
+//        let urlPHP = "http://androidthai.in.th"
+//        //Change String to url String
+//        let myURL = URL(string: urlPHP)
+//        
+//        let request = NSMutableURLRequest(url: myURL!)
+//        let task = URLSession.shared.dataTask(with: request as URLRequest){
+//        data, response, error in
+//        
+//            if error != nil {
+//            print("Error ==> \(String(describing: error))")
+//            }   else {
+//                
+//                if let unwrappedData = data{
+//                let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+//                let strJSON = dataString as Any
+//                print("strJSON ==> \(strJSON)")
+//            
+//                         }
+//                
+//        }// if1
+//        
+//    }
+//    task.resume()
+        
+        
+}//Main Method
+    
+    func readJSON(strURL: String) -> Void {
+        
         //Process Connected Http
-        let urlPHP = "http://androidthai.in.th"
+        let urlPHP = strURL
         //Change String to url String
         let myURL = URL(string: urlPHP)
         
         let request = NSMutableURLRequest(url: myURL!)
         let task = URLSession.shared.dataTask(with: request as URLRequest){
-        data, response, error in
-        
+            data, response, error in
+            
             if error != nil {
-            print("Error ==> \(String(describing: error))")
+                print("Error ==> \(String(describing: error))")
             }   else {
                 
                 if let unwrappedData = data{
-                let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                let strJSON = dataString as Any
-                print("strJSON ==> \(strJSON)")
-            
-                         }
-               
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    let strJSON = dataString as Any
+                    print("strJSON ==> \(strJSON)")
+                    
+                }
                 
-        }// if1
-        
+            }// if1
             
-    }   //Main Method
-    task.resume()
+        }
+        task.resume()
+
         
         
-}
+    } // read JSON
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
